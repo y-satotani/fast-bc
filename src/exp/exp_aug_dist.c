b2f6f35512a8503f44f0f2646dd529a29ac69dc1
ysatotani <yoshikimethod@hotmail.co.jp>
2019-06-21 15:05:04 +0900

    Add augmented distance and betweenness centrality experiments

--- src/exp/exp_aug_dist.c
+++ src/exp/exp_aug_dist.c
@@ -5,36 +5,42 @@
 #include <time.h>
 #include <assert.h>
 #include <igraph.h>
-#include "bcdec/arg_dist.h"
-#include "bcdec/update_on_insert.h"
-#include "bcdec/update_on_delete.h"
+#include "dybc/aug_dist.h"
+#include "dybc/incremental.h"
+#include "dybc/decremental.h"
 
-void make_graph(igraph_t* G, char* name, int n, int k);
+void make_graph(igraph_t* G, char* name, int n, int k, const char* weight);
 void choice_noadjacent_pair(igraph_t* G, int* u, int* v);
 
 int main(int argc, char* argv[]) {
   // Initialization
-  if(argc < 5) return 1;
+  if(argc < 6) return 1;
   igraph_t G;
-  char* name = argv[1];
-  int n = atoi(argv[2]), k = atoi(argv[3]);
-  char* mode = argv[4];
-  make_graph(&G, name, n, k);
+  long int seed = atoll(argv[1]);
+  char* name = argv[2];
+  int n = atoi(argv[3]), k = atoi(argv[4]);
+  char* mode = argv[5];
+  const char* weight = "length";
+  igraph_i_set_attribute_table(&igraph_cattribute_table);
+  igraph_rng_seed(igraph_rng_default(), seed);
+  make_graph(&G, name, n, k, weight);
 
-  // Make argumented distance
-  igraph_matrix_t D, Sigma;
-  arg_dist(&D, &Sigma, &G);
+  // Make augmented distance
+  igraph_matrix_t D;
+  igraph_matrix_int_t Sigma;
+  aug_dist(&G, &D, &Sigma, weight);
 
-  // Choose edge to insert/delete then update
+  // Choose edge to insert/delete
   igraph_integer_t u, v;
+  igraph_real_t c;
   if(strcmp(mode, "insert") == 0) {
     choice_noadjacent_pair(&G, &u, &v);
-    igraph_add_edge(&G, u, v);
+    c = igraph_rng_get_integer(igraph_rng_default(), 1, 5);
   } else if(strcmp(mode, "delete") == 0) {
     igraph_integer_t eid = igraph_rng_get_integer
       (igraph_rng_default(), 0, igraph_ecount(&G)-1);
     igraph_edge(&G, eid, &u, &v);
-    igraph_delete_edges(&G, igraph_ess_1(eid));
+    c = EAN(&G, weight, eid);
   } else assert(0);
 
   // Update
@@ -42,11 +48,11 @@ int main(int argc, char* argv[]) {
   double time_update;
   if(strcmp(mode, "insert") == 0) {
     start = clock();
-    update_on_insert(&D, &Sigma, u, v);
+    incremental(&G, u, v, c, &D, &Sigma, 0, weight);
     end = clock();
   } else if(strcmp(mode, "delete") == 0) {
     start = clock();
-    update_on_delete(&D, &Sigma, u, v);
+    decremental(&G, u, v, &D, &Sigma, 0, weight);
     end = clock();
   } else assert(0);
   time_update = (double)(end - start) / CLOCKS_PER_SEC;
@@ -54,27 +60,33 @@ int main(int argc, char* argv[]) {
   // Calculate training data
   igraph_matrix_t Dtrain;
   double time_igraph;
+  igraph_vector_t weights;
   igraph_matrix_init(&Dtrain, igraph_vcount(&G), igraph_vcount(&G));
+  igraph_vector_init(&weights, igraph_ecount(&G));
+  for(igraph_integer_t eid = 0; eid < igraph_ecount(&G); eid++) {
+    VECTOR(weights)[eid] = EAN(&G, weight, eid);
+  }
   start = clock();
-  igraph_shortest_paths(&G, &Dtrain,
-                        igraph_vss_all(), igraph_vss_all(), IGRAPH_ALL);
+  igraph_shortest_paths_dijkstra
+    (&G, &Dtrain, igraph_vss_all(), igraph_vss_all(), &weights, IGRAPH_ALL);
   end = clock();
   time_igraph = (double)(end - start) / CLOCKS_PER_SEC;
 
-  printf("%s,%d,%d,%s,%d,%f,%f\n",
-         name, n, k, mode,
-         igraph_matrix_all_e(&D, &Dtrain),
+  printf("%ld,%s,%d,%d,%s,%f,%f,%f\n",
+         seed, name, n, k, mode,
+         igraph_matrix_maxdifference(&D, &Dtrain),
          time_update, time_igraph);
 
+  igraph_vector_destroy(&weights);
   igraph_matrix_destroy(&D);
-  igraph_matrix_destroy(&Sigma);
+  igraph_matrix_int_destroy(&Sigma);
   igraph_matrix_destroy(&Dtrain);
   igraph_destroy(&G);
 
   return 0;
 }
 
-void make_graph(igraph_t* G, char* name, int n, int k) {
+void make_graph(igraph_t* G, char* name, int n, int k, const char* weight) {
   assert(k % 2 == 0);
   if(strcmp(name, "RRG") == 0)
     igraph_k_regular_game(G, n, k, 0, 0);
@@ -84,10 +96,16 @@ void make_graph(igraph_t* G, char* name, int n, int k) {
     igraph_barabasi_game(G, n, 1, k/2, 0, 1, 1, 0, IGRAPH_BARABASI_BAG, 0);
   else
     assert(0);
+  // set weights
+  for(igraph_integer_t eid = 0; eid < igraph_ecount(G); eid++) {
+    long int l = igraph_rng_get_integer(igraph_rng_default(), 1, 5);
+    SETEAN(G, weight, eid, l);
+  }
 }
 
 void choice_noadjacent_pair(igraph_t* G,
-                            igraph_integer_t* u, igraph_integer_t* v) {
+                            igraph_integer_t* u,
+                            igraph_integer_t* v) {
   igraph_vector_int_t ulist, vlist;
   igraph_integer_t w, x, eid;
   igraph_vector_int_init(&ulist, 0);
--- src/exp/par_argdist.py
+++ src/exp/par_argdist.py
@@ -2,8 +2,8 @@
 from itertools import chain, product
 
 print("\n".join([
-    "{} {} {} {}".format(gtype, n, k, qtype)
-    for _ in range(100)
+    "{} {} {} {} {}".format(seed, gtype, n, k, qtype)
+    for seed in range(100)
     for gtype in ('RRG', 'ER', 'BA')
     for n in chain(range(10, 100, 10), range(100, 1000+1, 100))
     for k in chain(range(4, 10, 2), range(10, 100+1, 10))
