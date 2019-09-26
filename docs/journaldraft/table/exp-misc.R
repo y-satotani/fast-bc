bcd23611d9fe59995f0db468ce43d7d1f03ede24
ysatotani <yoshikimethod@hotmail.co.jp>
2019-09-26 21:29:33 +0900

    Update draft of journal

---
+++ docs/journaldraft/table/exp-misc.R
@@ -0,0 +1,33 @@
+library(readr)
+library(tidyr)
+library(dplyr)
+library(xtable)
+
+out_file <- paste0(sub('^--file=(.+)\\.R$', '\\1', basename(commandArgs()[4])), '.tex')
+
+data_time <- read_csv('../../res/data/misc.csv') %>%
+  select(c('gml', 'time-proposed', 'time-brandes')) %>%
+  rename(network = gml) %>%
+  mutate(network = sub('.gml', '', basename(network))) %>%
+  gather(
+    key = 'method', value = 'time',
+    `time-proposed`, `time-brandes`
+  ) %>%
+  mutate(method = sub('time-', '', method)) %>%
+  group_by(network, method) %>%
+  summarise(mean = mean(time), max = max(time)) %>%
+  gather(Var, val, `mean`, `max`) %>%
+  unite(Var1, method, Var, sep = '-') %>%
+  spread(Var1, val) %>%
+  ungroup()
+
+print(
+  xtable(data_time,
+         caption = 'Execution time on real networks',
+         label = 'tab:exp-real'
+         ),
+  caption.placement = 'top',
+  include.rownames = FALSE,
+  file = out_file
+  )
+
