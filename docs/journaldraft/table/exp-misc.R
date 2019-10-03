9d10c024bdbb9b4d185f3171d496fb19e679c085
ysatotani <yoshikimethod@hotmail.co.jp>
2019-10-03 22:01:07 +0900

    Update misc.csv (add order and size columns)

--- docs/journaldraft/table/exp-misc.R
+++ docs/journaldraft/table/exp-misc.R
@@ -6,7 +6,7 @@ library(xtable)
 out_file <- paste0(sub('^--file=(.+)\\.R$', '\\1', basename(commandArgs()[4])), '.tex')
 
 data_time <- read_csv('../../res/data/misc.csv') %>%
-  select(c('gml', 'time-proposed', 'time-brandes')) %>%
+  select(c('order', 'size', 'gml', 'time-proposed', 'time-brandes')) %>%
   rename(network = gml) %>%
   mutate(network = sub('.gml', '', basename(network))) %>%
   gather(
@@ -14,7 +14,7 @@ data_time <- read_csv('../../res/data/misc.csv') %>%
     `time-proposed`, `time-brandes`
   ) %>%
   mutate(method = sub('time-', '', method)) %>%
-  group_by(network, method) %>%
+  group_by(network, order, size, method) %>%
   summarise(mean = mean(time), max = max(time)) %>%
   gather(Var, val, `mean`, `max`) %>%
   unite(Var1, method, Var, sep = '-') %>%
