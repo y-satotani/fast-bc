b4ab5752b53d9e44a5c7d20caf3ac248b3ab0cb0
leichtrhino <yoshikimethod@hotmail.co.jp>
2019-12-20 15:52:45 +0900

    Update program to plot result of update size/time experiments

--- docs/draft/plot/exp-artificial-path-update.R
+++ docs/draft/plot/exp-artificial-path-update.R
@@ -4,60 +4,132 @@ library(tidyr)
 library(ggplot2)
 library(viridis)
 library(latex2exp)
+library(cowplot)
 theme_set(theme_light(base_size = 9, base_family = 'IPAexGothic'))
 out_file <- paste0(sub('^--file=(.+)\\.R$', '\\1', basename(commandArgs()[4])), '.pdf')
 
-data_time <- read_csv('../../res/data/result-update-stats.csv') %>%
-  separate('network', c('topology', 'order', 'degree', NA, 'net-seed', NA)) %>%
-  mutate(order = as.numeric(order), degree = as.numeric(degree)) %>%
-  filter(topology == 'RRG') %>%
-  filter(`is-weighted` == 'weighted') %>%
-  filter(order == 1000) %>%
-  mutate(
-    topology = factor(
-      topology,
-      levels = c('RRG', 'BA'),
-      labels = c('ランダム正則グラフ', 'Barabási–Albertモデル')
-    ),
-    query = factor(
-      query,
-      levels = c('insert', 'delete'),
-      labels = c('挿入', '削除')
-    ),
+data_time <- read_csv('../../res/data/artificial-update-stats-time.csv') %>%
+    separate(
+        'network',
+        c('topology', 'order', 'degree', NA, 'net-seed', NA)
     ) %>%
-    mutate(`pred-time-path` = (`n-aff-src` + `n-aff-tgt`) * (degree * `upd-path` * log(`upd-path`)))
+    select(-c(`is-directed`)) %>%
+    mutate(order = as.numeric(order), degree = as.numeric(degree)) %>%
+    filter(topology == 'RRG', order == 1000, degree == 4) %>%
+    mutate(
+        `pred-time-path` = if_else(
+            `is-weighted` == 'weighted',
+            (`n-aff-src`+`n-aff-tgt`)*(degree*`upd-path`*log(`upd-path`)),
+            (`n-aff-src`+`n-aff-tgt`)*(degree*`upd-path`)
+        )
+    ) %>%
+    mutate(
+        topology = factor(
+            topology,
+            levels = c('RRG', 'BA'),
+            labels = c('ランダム正則グラフ', 'Barabási–Albertモデル')
+        ),
+        `is-weighted` = factor(
+            `is-weighted`,
+            levels = c('unweighted', 'weighted'),
+            labels = c('重みなし', '重み付き')
+        ),
+        query = factor(
+            query,
+            levels = c('insert', 'delete'),
+            labels = c('挿入', '削除')
+        )
+    )
 
 linear_models <- data_time %>%
     group_by(topology, order, degree, query, `is-weighted`) %>%
     do(broom::tidy(lm(`time-path` ~ `pred-time-path`, .))) %>%
     ungroup() %>%
-    select(c(topology, order, degree, degree, query, `is-weighted`, term, estimate)) %>%
+    select(c(topology, order, degree, query, `is-weighted`, term, estimate)) %>%
     spread(key = term, value = estimate) %>%
     rename(intercept = `(Intercept)`, slope = `\`pred-time-path\``) %>%
-    mutate(slope = sprintf('%.3e', slope)) %>%
-    mutate(intercept = sprintf('%.3e', intercept)) %>%
-    mutate(`line-label` = as.character(TeX(paste0('$\\mathit{y}=', slope, '\\mathit{x}+', intercept, '$'), output = 'expression')))
+    mutate(
+        `line-label` = as.character(TeX(paste0(
+            '$\\mathit{y}=',
+            sprintf('%.3e', slope),
+            '\\mathit{x}+',
+            sprintf('%.3e', intercept),
+            '$'), output = 'expression'))
+    ) %>%
+    left_join(
+        data_time %>%
+        group_by(topology, order, degree, query, `is-weighted`) %>%
+        summarise(x = max(`pred-time-path`, na.rm = TRUE)) %>%
+        ungroup(),
+        by = c("topology", "order", "degree", "query", "is-weighted")
+    )
+
 linear_models_glance <- data_time %>%
     group_by(topology, order, degree, query, `is-weighted`) %>%
     do(broom::glance(lm(`time-path` ~ `pred-time-path`, .)))
+linear_models_glance
 
-gp <- ggplot(
-    data_time %>% filter(degree %in% c(4, 16, 64)),
-    aes(`pred-time-path`, `time-path`, colour = factor(degree))
-  ) +
-    geom_point(alpha = 0.2) +
-    geom_text(data = linear_models, aes(x = 100000, y = 0, label = `line-label`), parse = TRUE, size = 3, colour = 'black', family = 'Times New Roman', face = 'italic') +
-    geom_smooth(method = 'lm', se = FALSE, colour = 'black', size = 0.2) +
-    guides(colour = guide_legend(title = '次数', override.aes = list(alpha = 1))) +
-    facet_grid(cols = vars(query)) +
-    xlab(TeX('$\\mathit{x}=(|\\mathit{S}(\\mathit{v})|+|\\mathit{T}(\\mathit{u})|)(\\mathit{k}\\bar{\\mathit{A}}+\\bar{\\mathit{A}}\\log\\bar{\\mathit{A}})$')) +
-    ylab(TeX('$\\mathit{y}$：実行時間(s)')) +
-  scale_colour_viridis(discrete = TRUE, begin = 0.8, end = 0.2) +
-  theme(
-    legend.position = 'top',
-    strip.text = element_text(colour = 'black'),
-    strip.background = element_blank(),
-    axis.title.x = element_text(family = 'Times New Roman', face = 'italic')
-  )
+x_label_weighted = TeX(paste0(
+    '$\\mathit{x}',
+    '=(|\\mathit{S}(\\mathit{v})|+|\\mathit{T}(\\mathit{u})|)',
+    '(\\mathit{k}\\bar{\\mathit{A}}+\\bar{\\mathit{A}}\\log\\bar{\\mathit{A}})$'
+))
+x_label_unweighted = TeX(paste0(
+    '$\\mathit{x}',
+    '=(|\\mathit{S}(\\mathit{v})|+|\\mathit{T}(\\mathit{u})|)',
+    '\\mathit{k}\\bar{\\mathit{A}}$'
+))
+y_label = TeX('$\\mathit{y}$：実行時間(s)')
 
-ggsave(out_file, gp, cairo_pdf, width = 12, height = 8, units = 'cm')
+make_gp = function(is_weighted_, query_) {
+    gp <- ggplot(
+        data_time %>% filter(`is-weighted` == is_weighted_, query == query_),
+        aes(`pred-time-path`, `time-path`, colour = factor(degree))
+    ) +
+        geom_point(alpha = 0.2) +
+        geom_text(
+            aes(x = x, y = 0, label = `line-label`),
+            data = linear_models %>%
+                filter(`is-weighted` == is_weighted_, query == query_),
+            parse = TRUE,
+            size = 3,
+            colour = 'black',
+            family = 'Times New Roman',
+            vjust = 0,
+            hjust = 1
+        ) +
+        geom_abline(
+            aes(slope = slope, intercept = intercept),
+            data = linear_models %>%
+                filter(`is-weighted` == is_weighted_, query == query_),
+            size = 0.25
+        ) +
+        guides(
+            colour = guide_legend(
+                title = '次数', override.aes = list(alpha = 1)
+            )
+        ) +
+        xlab(ifelse(
+            is_weighted_ == '重み付き', x_label_weighted, x_label_unweighted
+        )) +
+        ylab(y_label) +
+        ggtitle(paste(is_weighted_, query_)) +
+        scale_colour_viridis(discrete = TRUE, begin = 0.8, end = 0.2) +
+        theme(
+            legend.position = 'none',
+            plot.title = element_text(size = 10),
+            axis.title.x = element_text(family = 'Times New Roman')
+        )
+    return(gp)
+}
+
+gp1 = make_gp('重み付き', '挿入')
+gp2 = make_gp('重み付き', '削除')
+gp3 = make_gp('重みなし', '挿入')
+gp4 = make_gp('重みなし', '削除')
+gp <- plot_grid(
+    gp1, gp2, gp3, gp4,
+    ncol = 2,
+    label_size = 10
+)
+ggsave(out_file, gp, cairo_pdf, width = 15, height = 10, units = 'cm')
