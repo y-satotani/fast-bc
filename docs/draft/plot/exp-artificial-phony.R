5d7f1be89c22debdec5e271abe1b40846ef24394
leichtrhino <yoshikimethod@hotmail.co.jp>
2019-12-20 15:54:08 +0900

    Update program to plot result of redundant traversal

--- docs/draft/plot/exp-artificial-phony.R
+++ docs/draft/plot/exp-artificial-phony.R
@@ -3,40 +3,116 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(viridis)
+library(latex2exp)
+library(cowplot)
 theme_set(theme_light(base_size = 9, base_family = 'IPAexGothic'))
 out_file <- paste0(sub('^--file=(.+)\\.R$', '\\1', basename(commandArgs()[4])), '.pdf')
 
-data_time <- read_csv('../../res/data/artificial.csv') %>%
-  filter(name %in% c('ER', 'BA')) %>%
-  mutate(
-    `time-diff` = `time-brandes` - `time-proposed`,
-    `phony-verts` = `updated-bc-verts` - `changed-bc-verts`,
-    name = factor(
-      name,
-      levels = c('RRG', 'ER', 'BA'),
-      labels = c('ランダム正則グラフ', 'Erdős–Rényiモデル', 'Barabási–Albertモデル')
-    ),
-    mode = factor(
-      mode,
-      levels = c('insert', 'delete'),
-      labels = c('挿入', '削除')
-    ),
-    k = factor(k)
-  )
+data <- read_csv('../../res/data/artificial-update-stats-order.csv') %>%
+    separate(
+        'network',
+        c('topology', 'order', 'degree', NA, 'net-seed', NA)
+    ) %>%
+    select(-c(`is-directed`)) %>%
+    mutate(order = as.numeric(order), degree = as.numeric(degree)) %>%
+    filter(order == 1000, degree == 4) %>% # TODO: use all order
+    mutate(
+        topology = factor(
+            topology,
+            levels = c('RRG', 'BA'),
+            labels = c('ランダム正則グラフ', 'Barabási–Albertモデル')
+        ),
+        `is-weighted` = factor(
+            `is-weighted`,
+            levels = c('unweighted', 'weighted'),
+            labels = c('重みなし', '重み付き')
+        ),
+        query = factor(
+            query,
+            levels = c('insert', 'delete'),
+            labels = c('挿入', '削除')
+        )
+    )
 
-gp <- ggplot(
-    data_time %>% filter(n == 1000, k %in% c(4)),
-    aes(`changed-bc-verts`, `updated-bc-verts`, colour = mode)
-  ) +
-  geom_point(alpha = 0.2) +
-  guides(colour = guide_legend(title = '操作', override.aes = list(alpha = 1))) +
-  facet_grid(cols = vars(name)) +
-  xlab('媒介中心性が変化した頂点数') + ylab('媒介中心性を更新した頂点数') +
-  scale_colour_viridis(discrete = TRUE, begin = 0.8, end = 0.2) +
-  theme(
-    legend.position = 'top',
-    strip.text = element_text(colour = 'black'),
-    strip.background = element_blank()
-  )
+max_slope <- data %>%
+    group_by(topology, order, degree, query, `is-weighted`) %>%
+    summarise(slope = max(`tau-hat` / `tau-ast`, na.rm = TRUE)) %>%
+    ungroup() %>%
+    mutate(
+        `line-label` = as.character(TeX(paste0(
+            '$\\mathit{\\hat{\\tau}}=',
+            sprintf('%.3e', slope),
+            '\\mathit{\\tau *}',
+            '$'), output = 'expression'))
+    ) %>%
+    left_join(
+        data %>%
+        group_by(topology, order, degree, query, `is-weighted`) %>%
+        summarise(x = max(`tau-ast`, na.rm = TRUE)) %>%
+        ungroup(),
+        by = c("topology", "order", "degree", "query", "is-weighted")
+    )
 
-ggsave(out_file, gp, cairo_pdf, width = 12, height = 8, units = 'cm')
+make_gp = function(topology_, is_weighted_, query_) {
+    gp <- ggplot(
+        data %>%
+        filter(
+            topology == topology_,
+            `is-weighted` == is_weighted_,
+            query == query_
+        ),
+        aes(`tau-ast`, `tau-hat`, colour = factor(degree))
+    ) +
+        geom_point(alpha = 0.2) +
+        geom_text(
+            aes(x = x, y = 0, label = `line-label`),
+            data = max_slope %>%
+                filter(
+                    topology == topology_,
+                    `is-weighted` == is_weighted_,
+                    query == query_
+                ),
+            parse = TRUE,
+            size = 3,
+            colour = 'black',
+            family = 'Times New Roman',
+            vjust = 0,
+            hjust = 1
+        ) +
+        geom_abline(
+            aes(slope = slope, intercept = 0),
+            data = max_slope %>%
+                filter(
+                    topology == topology_,
+                    `is-weighted` == is_weighted_,
+                    query == query_
+                ),
+            size = 0.25
+        ) +
+        xlab(expression(tau*"*")) +
+        ylab(TeX('$\\mathit{\\hat{\\tau}}$')) +
+        xlim(0, max(data$order)) +
+        ggtitle(paste(is_weighted_, topology_, query_)) +
+        scale_colour_viridis(discrete = TRUE, begin = 0.8, end = 0.2) +
+        theme(
+            legend.position = 'none',
+            plot.title = element_text(size = 10),
+            axis.title.x = element_text(family = 'Times New Roman'),
+            axis.title.y = element_text(family = 'Times New Roman')
+        )
+    return(gp)
+}
+
+                                        #is_weighted = '重み付き'
+                                        #is_weighted = '重みなし'
+is_weighted = '重み付き'
+gp1 = make_gp('ランダム正則グラフ', is_weighted, '挿入')
+gp2 = make_gp('ランダム正則グラフ', is_weighted, '削除')
+gp3 = make_gp('Barabási–Albertモデル', is_weighted, '挿入')
+gp4 = make_gp('Barabási–Albertモデル', is_weighted, '削除')
+gp <- plot_grid(
+    gp1, gp2, gp3, gp4,
+    ncol = 2,
+    label_size = 10
+)
+ggsave(out_file, gp, cairo_pdf, width = 15, height = 10, units = 'cm')
