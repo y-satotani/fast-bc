39fe33c4a0e34a92e9355078fcb59313a64ad208
leichtrhino <yoshikimethod@hotmail.co.jp>
2019-12-22 18:31:08 +0900

    Update algorithm for updating shortest path on increasing weight of an edge

--- src/dybc/decremental_shortest_path.c
+++ src/dybc/decremental_shortest_path.c
@@ -17,14 +17,22 @@ void update_sssp_dec_weighted(igraph_t* G,
                               igraph_vector_t* weights,
                               igraph_real_t weight,
                               igraph_bool_t is_post_mod) {
+
+  // edge getting modification
+  igraph_integer_t eid_;
+  igraph_get_eid(G, &eid_, u, v, 1, 0);
+  assert(eid_ >= 0 && "edge does not exist");
+
 #define EPS IGRAPH_SHORTEST_PATH_EPSILON
 #define cmp(a, b) (igraph_cmp_epsilon((a), (b), EPS))
 #define d(a, b) (MATRIX(*D, (a), (b)))
 #define s(a, b) (MATRIX(*S, (a), (b)))
 #define l(eid) (weights ? VECTOR(*weights)[eid] : 1)
+#define lp(eid) (eid == eid_ ? weight : (weights ? VECTOR(*weights)[eid] : 1))
 
+  // early out: no effect if d_sv < d_su + l_uv
   if(igraph_is_inf(d(source, u))
-     || cmp(d(source, v), d(source, u) + weight) < 0) {
+     || cmp(d(source, v), d(source, u) + l(eid_)) < 0) {
     return;
   }
 
@@ -41,12 +49,12 @@ void update_sssp_dec_weighted(igraph_t* G,
   for(long int xi = 0; xi < igraph_vector_int_size(&targets); xi++) {
     igraph_integer_t x = igraph_vector_int_e(&targets, xi);
     igraph_vector_int_t* ys = igraph_inclist_get(preds, x);
-    igraph_real_t d_hat = IGRAPH_INFINITY;
+    igraph_real_t d_hat = d(source, x) + weight - l(eid_);
     for(long int yi = 0; yi < igraph_vector_int_size(ys); yi++) {
       igraph_integer_t eid = igraph_vector_int_e(ys, yi);
       igraph_integer_t y = IGRAPH_OTHER(G, eid, x);
-      if(!VECTOR(is_affected)[y] && cmp(d(source, y) + l(eid), d_hat) < 0)
-        d_hat = d(source, y) + l(eid);
+      if(!VECTOR(is_affected)[y] && cmp(d(source, y) + lp(eid), d_hat) < 0)
+        d_hat = d(source, y) + lp(eid);
     }
     if(igraph_is_inf(d_hat)) {
       d(source, x) = IGRAPH_INFINITY;
@@ -67,7 +75,7 @@ void update_sssp_dec_weighted(igraph_t* G,
       igraph_integer_t eid = VECTOR(*ps)[pi];
       igraph_integer_t p = IGRAPH_OTHER(G, eid, x);
       if(!VECTOR(is_affected)[p]
-         && cmp(d(source, x), d(source, p) + l(eid)) == 0)
+         && cmp(d(source, x), d(source, p) + lp(eid)) == 0)
         s(source, x) += s(source, p);
     } /* for preds */
 
@@ -75,7 +83,7 @@ void update_sssp_dec_weighted(igraph_t* G,
     for(long int si = 0; si < igraph_vector_int_size(ss); si++) {
       igraph_integer_t eid = VECTOR(*ss)[si];
       igraph_integer_t s = IGRAPH_OTHER(G, eid, x);
-      igraph_real_t d_s = d(source, x) + l(eid);
+      igraph_real_t d_s = d(source, x) + lp(eid);
       if(VECTOR(is_affected)[s]) {
         if(!igraph_2wheap_has_elem(&queue, s))
           igraph_2wheap_push_with_index(&queue, s, -d_s);
@@ -93,6 +101,7 @@ void update_sssp_dec_weighted(igraph_t* G,
 #undef d
 #undef s
 #undef l
+#undef lp
 }
 
 void update_stsp_dec_weighted(igraph_t* G,
@@ -106,17 +115,25 @@ void update_stsp_dec_weighted(igraph_t* G,
                               igraph_vector_t* weights,
                               igraph_real_t weight,
                               igraph_bool_t is_post_mod) {
+
+  // edge getting modification
+  igraph_integer_t eid_;
+  igraph_get_eid(G, &eid_, u, v, 1, 0);
+  assert(eid_ >= 0 && "edge does not exist");
+
 #define EPS IGRAPH_SHORTEST_PATH_EPSILON
 #define cmp(a, b) (igraph_cmp_epsilon((a), (b), EPS))
 #define d(a, b) (MATRIX(*D, (a), (b)))
 #define s(a, b) (MATRIX(*S, (a), (b)))
 #define l(eid) (weights ? VECTOR(*weights)[eid] : 1)
+#define lp(eid) (eid == eid_ ? weight : (weights ? VECTOR(*weights)[eid] : 1))
 
   if(igraph_is_inf(d(v, target))
-     || cmp(d(u, target), weight + d(v, target)) < 0) {
+     || cmp(d(u, target), l(eid_) + d(v, target)) < 0) {
     return;
   }
 
+  // find affected sources
   igraph_vector_int_t sources, is_affected;
   igraph_vector_int_init(&sources, 0);
   igraph_vector_int_init(&is_affected, igraph_vcount(G));
@@ -130,12 +147,12 @@ void update_stsp_dec_weighted(igraph_t* G,
   for(long int xi = 0; xi < igraph_vector_int_size(&sources); xi++) {
     igraph_integer_t x = igraph_vector_int_e(&sources, xi);
     igraph_vector_int_t* ys = igraph_inclist_get(succs, x);
-    igraph_real_t d_hat = IGRAPH_INFINITY;
+    igraph_real_t d_hat = d(x, target) + weight - l(eid_);
     for(long int yi = 0; yi < igraph_vector_int_size(ys); yi++) {
       igraph_integer_t eid = igraph_vector_int_e(ys, yi);
       igraph_integer_t y = IGRAPH_OTHER(G, eid, x);
-      if(!VECTOR(is_affected)[y] && cmp(l(eid) + d(y, target), d_hat) < 0)
-        d_hat = l(eid) + d(y, target);
+      if(!VECTOR(is_affected)[y] && cmp(lp(eid) + d(y, target), d_hat) < 0)
+        d_hat = lp(eid) + d(y, target);
     }
     if(igraph_is_inf(d_hat)) {
       d(x, target) = IGRAPH_INFINITY;
@@ -154,7 +171,7 @@ void update_stsp_dec_weighted(igraph_t* G,
     for(long int si = 0; si < igraph_vector_int_size(ss); si++) {
       igraph_integer_t eid = VECTOR(*ss)[si];
       igraph_integer_t s = IGRAPH_OTHER(G, eid, x);
-      if(cmp(d(x, target), l(eid) + d(s, target)) == 0)
+      if(cmp(d(x, target), lp(eid) + d(s, target)) == 0)
         s(x, target) += s(s, target);
     } /* for succs */
 
@@ -162,7 +179,7 @@ void update_stsp_dec_weighted(igraph_t* G,
     for(long int pi = 0; pi < igraph_vector_int_size(ps); pi++) {
       igraph_integer_t eid = VECTOR(*ps)[pi];
       igraph_integer_t p = IGRAPH_OTHER(G, eid, x);
-      igraph_real_t d_p = l(eid) + d(x, target);
+      igraph_real_t d_p = lp(eid) + d(x, target);
       if(VECTOR(is_affected)[p]) {
         if(!igraph_2wheap_has_elem(&queue, p))
           igraph_2wheap_push_with_index(&queue, p, -d_p);
@@ -180,6 +197,7 @@ void update_stsp_dec_weighted(igraph_t* G,
 #undef d
 #undef s
 #undef l
+#undef lp
 }
 
 void update_sssp_dec_unweighted(igraph_t* G,
@@ -191,13 +209,21 @@ void update_sssp_dec_unweighted(igraph_t* G,
                                 igraph_integer_t v,
                                 igraph_integer_t source,
                                 igraph_bool_t is_post_mod) {
+
+  // edge getting modification
+  igraph_integer_t eid_;
+  igraph_get_eid(G, &eid_, u, v, 1, 0);
+  assert(eid_ >= 0 && "edge does not exist");
+
 #define EPS IGRAPH_SHORTEST_PATH_EPSILON
 #define cmp(a, b) (igraph_cmp_epsilon((a), (b), EPS))
 #define d(a, b) (MATRIX(*D, (a), (b)))
 #define s(a, b) (MATRIX(*S, (a), (b)))
+#define l(eid) 1.0
+#define lp(eid) (eid == eid_ ? IGRAPH_INFINITY : 1.0)
 
   if(igraph_is_inf(d(source, u))
-     || cmp(d(source, v), d(source, u) + 1.0) < 0) {
+     || cmp(d(source, v), d(source, u) + l(eid_)) < 0) {
     return;
   }
 
@@ -216,23 +242,25 @@ void update_sssp_dec_unweighted(igraph_t* G,
   for(long int xi = 0; xi < igraph_vector_int_size(&targets); xi++) {
     igraph_integer_t x = VECTOR(targets)[xi];
     igraph_vector_int_t* ys = igraph_inclist_get(preds, x);
-    igraph_real_t d_hat = IGRAPH_INFINITY;
-    igraph_real_t s_hat = 0;
+    igraph_real_t d_hat = d(source, x) + lp(eid_) - l(eid_);
+    igraph_integer_t s_hat = 0;
     for(long int yi = 0; yi < igraph_vector_int_size(ys); yi++) {
       igraph_integer_t eid = igraph_vector_int_e(ys, yi);
       igraph_integer_t y = IGRAPH_OTHER(G, eid, x);
       if(VECTOR(is_affected)[y]) continue;
-      if(cmp(d(source, y) + 1.0, d_hat) < 0) {
-        d_hat = d(source, y) + 1.0;
+      if(cmp(d(source, y) + lp(eid), d_hat) < 0) {
+        d_hat = d(source, y) + lp(eid);
         s_hat = 0;
       }
-      if(cmp(d(source, y) + 1.0, d_hat) == 0)
+      if(cmp(d(source, y) + lp(eid), d_hat) == 0)
         s_hat += s(source, y);
     } // end for each y in neighbor of x
+    if(!igraph_is_inf(d_hat))
+      igraph_2wbheap_push_with_index(&queue, x, (int)d_hat);
+    else
+      s_hat = 0;
     d(source, x) = d_hat;
     s(source, x) = s_hat;
-    if(!igraph_is_inf(d_hat))
-      igraph_2wbheap_push_with_index(&queue, x, (int)d(source, x));
   } // end for each affected target x
   for(long int xi = 0; xi < igraph_vector_int_size(&targets); xi++)
     if(!igraph_is_inf(d(source, VECTOR(targets)[xi])))
@@ -249,15 +277,15 @@ void update_sssp_dec_unweighted(igraph_t* G,
       igraph_integer_t eid = VECTOR(*neis)[ni];
       igraph_integer_t y = IGRAPH_OTHER(G, eid, x);
 
-      if(cmp(d(source, x) + 1.0, d(source, y)) < 0) {
-        d(source, y) = d(source, x) + 1.0;
+      if(cmp(d(source, x) + lp(eid), d(source, y)) < 0) {
+        d(source, y) = d(source, x) + lp(eid);
         s(source, y) = s(source, x);
         if(!igraph_2wbheap_has_elem(&queue, y)) {
           igraph_2wbheap_push_with_index(&queue, y, (int)d(source, y));
         } else if(cmp((int)d(source, y), igraph_2wbheap_get(&queue, y)) < 0) {
           igraph_2wbheap_modify(&queue, y, (int)d(source, y));
         }
-      } else if(cmp(d(source, x) + 1.0, d(source, y)) == 0) {
+      } else if(cmp(d(source, x) + lp(eid), d(source, y)) == 0) {
         s(source, y) += s(source, x);
       } // end if(d_sx+l_xy == d_sy)
     } // end for all y in neighbors
@@ -271,6 +299,8 @@ void update_sssp_dec_unweighted(igraph_t* G,
 #undef cmp
 #undef d
 #undef s
+#undef l
+#undef lp
 }
 
 void update_stsp_dec_unweighted(igraph_t* G,
@@ -282,13 +312,20 @@ void update_stsp_dec_unweighted(igraph_t* G,
                                 igraph_integer_t v,
                                 igraph_integer_t target,
                                 igraph_bool_t is_post_mod) {
+  // edge getting modification
+  igraph_integer_t eid_;
+  igraph_get_eid(G, &eid_, u, v, 1, 0);
+  assert(eid_ >= 0 && "edge does not exist");
+
 #define EPS IGRAPH_SHORTEST_PATH_EPSILON
 #define cmp(a, b) (igraph_cmp_epsilon((a), (b), EPS))
 #define d(a, b) (MATRIX(*D, (a), (b)))
 #define s(a, b) (MATRIX(*S, (a), (b)))
+#define l(eid) 1
+#define lp(eid) (eid == eid_ ? IGRAPH_INFINITY : 1)
 
   if(igraph_is_inf(d(u, target))
-     || cmp(d(v, target), d(u, target) + 1.0) < 0) {
+     || cmp(d(u, target), l(eid_) + d(v, target)) < 0) {
     return;
   }
 
@@ -307,23 +344,25 @@ void update_stsp_dec_unweighted(igraph_t* G,
   for(long int xi = 0; xi < igraph_vector_int_size(&sources); xi++) {
     igraph_integer_t x = VECTOR(sources)[xi];
     igraph_vector_int_t* ys = igraph_inclist_get(succs, x);
-    igraph_real_t d_hat = IGRAPH_INFINITY;
-    igraph_real_t s_hat = 0;
+    igraph_real_t d_hat = d(x, target) + lp(eid_) - l(eid_);
+    igraph_integer_t s_hat = 0;
     for(long int yi = 0; yi < igraph_vector_int_size(ys); yi++) {
       igraph_integer_t eid = igraph_vector_int_e(ys, yi);
       igraph_integer_t y = IGRAPH_OTHER(G, eid, x);
       if(VECTOR(is_affected)[y]) continue;
-      if(cmp(1.0 + d(y, target), d_hat) < 0) {
-        d_hat = 1.0 + d(y, target);
+      if(cmp(lp(eid) + d(y, target), d_hat) < 0) {
+        d_hat = lp(eid) + d(y, target);
         s_hat = 0;
       }
-      if(cmp(1.0 + d(y, target), d_hat) == 0)
+      if(cmp(lp(eid) + d(y, target), d_hat) == 0)
         s_hat += s(y, target);
     } // end for each y in neighbor of x
+    if(!igraph_is_inf(d_hat))
+      igraph_2wbheap_push_with_index(&queue, x, (int)d_hat);
+    else
+      s_hat = 0;
     d(x, target) = d_hat;
     s(x, target) = s_hat;
-    if(!igraph_is_inf(d_hat))
-      igraph_2wbheap_push_with_index(&queue, x, (int)d(x, target));
   } // end for each affected target x
   for(long int xi = 0; xi < igraph_vector_int_size(&sources); xi++)
     if(!igraph_is_inf(d(VECTOR(sources)[xi], target)))
@@ -340,15 +379,15 @@ void update_stsp_dec_unweighted(igraph_t* G,
       igraph_integer_t eid = VECTOR(*neis)[ni];
       igraph_integer_t y = IGRAPH_OTHER(G, eid, x);
 
-      if(cmp(1.0 + d(x, target), d(y, target)) < 0) {
-        d(y, target) = 1.0 + d(x, target);
+      if(cmp(lp(eid) + d(x, target), d(y, target)) < 0) {
+        d(y, target) = lp(eid) + d(x, target);
         s(y, target) = s(x, target);
         if(!igraph_2wbheap_has_elem(&queue, y)) {
           igraph_2wbheap_push_with_index(&queue, y, (int)d(y, target));
         } else if(cmp((int)d(y, target), igraph_2wbheap_get(&queue, y)) < 0) {
           igraph_2wbheap_modify(&queue, y, (int)d(y, target));
         }
-      } else if(cmp(1.0 + d(x, target), d(y, target)) == 0) {
+      } else if(cmp(lp(eid) + d(x, target), d(y, target)) == 0) {
         s(y, target) += s(x, target);
       } // end if(d_sx+l_xy == d_sy)
     } // end for all y in neighbors
@@ -362,6 +401,8 @@ void update_stsp_dec_unweighted(igraph_t* G,
 #undef cmp
 #undef d
 #undef s
+#undef l
+#undef lp
 }
 
 void affected_targets_dec(igraph_t* G,
@@ -378,15 +419,24 @@ void affected_targets_dec(igraph_t* G,
 #define cmp(a, b) (igraph_cmp_epsilon((a), (b), EPS))
 #define d(a, b) (MATRIX(*D, (a), (b)))
 #define l(eid) (weights ? VECTOR(*weights)[eid] : 1)
-  if(!weights) weight = 1;
-  assert(weight > 0);
-  assert(out);
 
+  // weight setting
+  if(!weights) weight = IGRAPH_INFINITY;
+  assert(weight > 0);
+
+  // clear output
+  assert(out);
   igraph_vector_int_clear(out);
+
+  // early out: no effect if d_sv < d_su + l_uv
+  igraph_integer_t eid_;
+  igraph_get_eid(G, &eid_, u, v, 1, 0);
+  assert(eid_ >= 0 && "edge does not exist");
   if(igraph_is_inf(d(source, u))
-     || cmp(d(source, v), d(source, u) + weight) < 0) {
+     || cmp(d(source, v), d(source, u) + l(eid_)) < 0) {
     return;
   }
+
   igraph_vector_bool_t visited;
   igraph_dqueue_int_t queue;
   igraph_vector_bool_init(&visited, igraph_vcount(G));
@@ -433,15 +483,25 @@ void affected_sources_dec(igraph_t* G,
 #define cmp(a, b) (igraph_cmp_epsilon((a), (b), EPS))
 #define d(a, b) (MATRIX(*D, (a), (b)))
 #define l(eid) (weights ? VECTOR(*weights)[eid] : 1)
-  if(!weights) weight = 1;
-  assert(weight > 0);
-  assert(out);
 
+  // weight setting
+  if(!weights) weight = IGRAPH_INFINITY;
+  assert(weight > 0);
+
+  // clear output
+  assert(out);
   igraph_vector_int_clear(out);
+
+  // early out: no effect if d_ut < l_uv + d_vt
+  igraph_integer_t eid_;
+  igraph_get_eid(G, &eid_, u, v, 1, 0);
+  assert(eid_ >= 0 && "edge does not exist");
   if(igraph_is_inf(d(v, target))
-     || cmp(d(u, target), weight + d(v, target)) < 0) {
+     || cmp(d(u, target), l(eid_) + d(v, target)) < 0) {
     return;
   }
+
+  // initializing
   igraph_vector_bool_t visited;
   igraph_dqueue_int_t queue;
   igraph_vector_bool_init(&visited, igraph_vcount(G));
