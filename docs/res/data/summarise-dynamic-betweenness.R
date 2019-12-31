936c0f0d8f4f101da6829a3a2eb6a1397e4ecb6e
leichtrhino <yoshikimethod@hotmail.co.jp>
2020-01-01 00:11:41 +0900

    Add aggregator for road network

---
+++ docs/res/data/summarise-dynamic-betweenness.R
@@ -0,0 +1,52 @@
+
+library(parallel)
+library(readr)
+library(dplyr)
+
+find_statistics <- function(file) {
+    library(readr)
+    library(dplyr)
+    untar(tarfile, files=c(file))
+    data <- read_csv(
+        file,
+        col_names = c('query', 'ev1', 'ev2', 'ex', 'bc'),
+        col_types = cols()
+    ) %>%
+        mutate(eu = pmin(ev1, ev2), ev = pmax(ev1, ev2)) %>%
+        select(-c(ev1, ev2)) %>%
+        inner_join(elist_to_graphml, by = c('eu' = 'ev')) %>% rename(u = gv) %>%
+        inner_join(elist_to_graphml, by = c('ev' = 'ev')) %>% rename(v = gv) %>%
+        inner_join(elist_to_graphml, by = c('ex' = 'ev')) %>% rename(x = gv) %>%
+        select(-c(eu, ev, ex)) %>%
+        mutate(bc = bc / ((n() - 1) * (n() - 2))) %>%
+        rename(`bc-after` = bc) %>%
+        inner_join(vertex_betweenness, by = c('x' = 'vert')) %>%
+        group_by(query, u, v) %>%
+        summarise(
+            `bc-delta-min` = min(`bc-after` - bc),
+            `bc-delta-max` = max(`bc-after` - bc),
+            `bc-delta-mean` = mean(`bc-after` - bc)
+        ) %>%
+        ungroup()
+    file.remove(file)
+    return(data)
+}
+
+data_dir <- '../../../docs/res/data/'
+vertex_betweenness <- read_csv(
+    paste0(data_dir, 'road-oka-vertex-betweenness.csv')
+)
+elist_to_graphml <- read_csv(
+    paste0(data_dir, 'road-oka-edgelist-to-graphml.csv'),
+    col_names = c('ev', 'gv')
+)
+tarfile <- paste0(data_dir, 'road-oka-dynamic.tar.gz')
+
+cl <- makeCluster(detectCores())
+clusterExport(cl, c('tarfile', 'vertex_betweenness', 'elist_to_graphml'))
+
+files <- untar(tarfile, list = TRUE)
+data <- parLapply(cl, files, find_statistics) %>% bind_rows()
+
+stopCluster(cl)
+write_csv(data, 'road-oka-betweenness-delta.csv')
