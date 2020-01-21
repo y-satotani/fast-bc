efc730d44531f99f306a0553f5b9ef154681afd3
leichtrhino <yoshikimethod@hotmail.co.jp>
2020-01-21 10:11:29 +0900

    Update thesis (this: rev3)

--- docs/thesis2020/table/exp-artificial.R
+++ docs/thesis2020/table/exp-artificial.R
@@ -14,14 +14,17 @@ data <- read_csv('../../res/data/artificial-performance-comparison.csv') %>%
     filter(query == 'delete', order %in% c(500, 1000)) %>%
     group_by(topology, order, degree, query, `is-weighted`) %>%
     summarise(
-        `time-brandes` = mean(`time-brandes`),
-        `time-proposed` = mean(`time-proposed`),
+        `Brandes'-mean` = mean(`time-brandes`),
+        `Brandes'-max` = max(`time-brandes`),
+        `proposed-mean` = mean(`time-proposed`),
+        `proposed-max` = max(`time-proposed`),
         speedup = mean(`time-brandes` / `time-proposed`)
     ) %>%
     ungroup() %>%
     select(-c(query)) %>%
     select(c(topology, `is-weighted`, order, degree,
-             `time-brandes`, `time-proposed`,
+             `Brandes'-mean`, `Brandes'-max`,
+             `proposed-mean`, `proposed-max`,
              speedup)) %>%
     arrange(topology, `is-weighted`, order, degree) %>%
     mutate(`is-weighted` = if_else(`is-weighted` == 'unweighted', '重みなし', '重み付き')) %>%
@@ -30,8 +33,10 @@ data <- read_csv('../../res/data/artificial-performance-comparison.csv') %>%
         `重み` = `is-weighted`,
         `頂点数` = `order`,
         `次数` = `degree`,
-        `Brandes法` = `time-brandes`,
-        `提案手法` = `time-proposed`,
+        `Brandes法-平均` = `Brandes'-mean`,
+        `Brandes法-最大` = `Brandes'-max`,
+        `提案手法-平均` = `proposed-mean`,
+        `提案手法-最大` = `proposed-max`,
         `性能比` = `speedup`
     )
 
@@ -39,7 +44,7 @@ print(
     xtable(data,
            caption = '人工ネットワーク上での性能比較(秒)',
            label = 'tab:exp-artificial',
-           digits = c(0, 0, 0, 0, 0, 3, 3, 2)
+           digits = c(0, 0, 0, 0, 0, 3, 3, 3, 3, 2)
            ),
     size = '\\fontsize{6pt}{8pt}\\selectfont',
     caption.placement = 'top',
